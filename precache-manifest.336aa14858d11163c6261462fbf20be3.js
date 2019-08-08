2d5c2b94341df34b63315ba6c3213138917c7bd4
Your Name <you@example.com>
2019-08-08 06:48:49 +0200

    Updates

---
+++ precache-manifest.336aa14858d11163c6261462fbf20be3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b6f9bb22f4943b720780b9fd2ec01a",
+    "url": "/services/index.html"
+  },
+  {
+    "revision": "644f6a076e7b9963ff0b",
+    "url": "/services/static/css/main.9bf9577c.chunk.css"
+  },
+  {
+    "revision": "f49f49a68cae45f36850",
+    "url": "/services/static/js/2.7be4d342.chunk.js"
+  },
+  {
+    "revision": "644f6a076e7b9963ff0b",
+    "url": "/services/static/js/main.9a1130d1.chunk.js"
+  },
+  {
+    "revision": "ea101cb10053e8d1d69d",
+    "url": "/services/static/js/runtime~main.e1bfea3b.js"
+  }
+]);
